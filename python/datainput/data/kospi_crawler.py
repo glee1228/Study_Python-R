b8b4a046546e8c135fbccd6094b4562d4edb0cfc
glee1228 <glee1228@users.noreply.github.com>
2018-08-27 15:17:26 +0900

    kospi crawler daily practice code

---
+++ python/datainput/data/kospi_crawler.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = 'https://finance.naver.com/sise'
+res = requests.get(url)
+result = BeautifulSoup(res.content, 'html.parser')
+kospi = result.select_one('#KOSPI_now')
+num = str(kospi)
+print(num)
+
+r1 = re.compile('[0-9][,][0-9][0-9][0-9][.][0-9][0-9]')
+print(r1.findall(num))
+result = r1.findall(num)
+print('현재 코스피 지수는 {}입니다.'.format(result))
+
